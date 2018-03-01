418fb9eed0134ceb530585d39387155639a43963
Steven Haws <stevenjhaws@gmail.com>
2018-02-28 17:43:06 -0700

    finished scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :accounts
+  has_one :profile
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
